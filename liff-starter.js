afbf36793dfd1dca4d390f1d3b88e070d01479bb
arwutr <arwutr@hotmail.com>
2019-10-23 13:18:38 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -147,7 +147,7 @@ function makeSticker(){
                  imageUrl: "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+ep,
                  action: {
                     type: "audio",
-                    audio: https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/android/sticker_sound.m4a,
+                    audio: "https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/android/sticker_sound.m4a",
                      action: {
                      type: "uri",
                      uri: "line://shop/sticker/detail/"+pkg}}}
