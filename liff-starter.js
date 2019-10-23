8190aaaa9f793f18806f3c3f8028d1bb1f7e8e31
arwutr <arwutr@hotmail.com>
2019-10-23 13:22:06 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -146,9 +146,8 @@ function makeSticker(){
              columns: [{
                  imageUrl: "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+ep,
                  action: {
-                    type: "audio",
-                    audio: "https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/android/sticker_sound.m4a",
-                     action: {
+                   audio: "https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/android/sticker_sound.m4a",
+                   action: {
                      type: "uri",
                      uri: "line://shop/sticker/detail/"+pkg}}}
                           ]
